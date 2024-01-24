ea6eb15049542faed5b9f5684b5e8c6b2054301b
Toufiqul <fahim.prime@gmail.com>
2024-01-25 01:52:20 +0600

    new way

--- src/CandleFlame.js
+++ src/CandleFlame.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import styled, { keyframes } from 'styled-components';
-
-const flicker = keyframes`
-  0%, 100% {
-    transform: translateY(0);
-  }
-  50% {
-    transform: translateY(-5px);
-  }
-`;
-
-const Flame = styled.div`
-  width: 10px;
-  height: 100px;
-  background: #ff5722;
-  border-radius: 5px;
-  position: relative;
-  animation: ${flicker} 0.5s infinite alternate;
-`;
+import './candleFlame.css';
 
 const CandleFlame = () => {
-  return <Flame />;
+  return <div className="candle-flame" />;
 };
 
 export default CandleFlame;
