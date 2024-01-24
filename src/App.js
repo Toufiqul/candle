05606aa63fcff1230f99f8e7183dc6c8727bc7b1
Toufiqul <fahim.prime@gmail.com>
2024-01-25 04:42:23 +0600

    bug: for (let i = 0; i < 7; i++)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React from 'react';
 import CandleFlame from './CandleFlame';
 
 function App() {
+    const repeatedElement = (element, times) => Array(times).fill(element);
+
+
+// Creating a list with five entries of the number 42
+const listOf42s = repeatedElement(<CandleFlame />, 7);
+function func(){
+    listOf42s.forEach(element =>{
+        return element;
+    });
+}
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <CandleFlame />
+        
+        for (let i = 0; i < 7; i++) {
+    setTimeout(()=>{return <CandleFlame/>}, i * 1000);
+}
+      
     </div>
   );
 }
